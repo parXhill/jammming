dcaa746025e5b8e97730fc119c581d1d37731878
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-07-27 16:25:17 +1000

    API key works now in .env, search bar input returning correct object

--- src/Search/SearchContainer.js
+++ src/Search/SearchContainer.js
@@ -3,6 +3,8 @@ import SearchBar from "./SearchBar";
 import Results from "./Results";
 
 const accessToken = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+console.log(accessToken);
+
 
 export default function SearchContainer() {
 
@@ -16,8 +18,10 @@ export default function SearchContainer() {
         e.preventDefault()
         const searchQuery = e.target.elements.query.value;
         console.log("Form submitted with input:", searchQuery);
+        
+        const encodedSearchQuery = `https://api.spotify.com/v1/search?q=${encodeURIComponent(searchQuery)}&type=track&market=AU&limit=10&offset=0`;
 
-        fetch('https://api.spotify.com/v1/search?q=gaga&type=track&market=AU&limit=10&offset=0', {
+        fetch(encodedSearchQuery, {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${accessToken}`
@@ -30,7 +34,11 @@ export default function SearchContainer() {
         return response.json();
         })
         .then(data => {
-        console.log(data); // Handle the response data
+            const trackNames = data.tracks.items.map(track => track.name);
+            const artistNames = data.tracks.items.map(track => track.artists[0].name);
+            console.log(trackNames);
+            console.log(artistNames);
+            //console.log(data); // Handle the response data
         })
         .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
---
+++ src/Search/testignore.js
@@ -0,0 +1,19 @@
+var client_id = '6d5485b0793d4962989d3e2abe7138ca';
+var client_secret = '415d605f93904bbea0f739eec8eb49ee';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
